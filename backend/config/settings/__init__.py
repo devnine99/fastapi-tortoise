0ce6043c9e8a31188056649a35b08acee3392a17
devnine99 <devnine99@gmail.com>
2021-12-12 23:54:56 +0900

    initial commit

--- backend/config/settings/__init__.py
+++ backend/config/settings/__init__.py
@@ -1,25 +1,20 @@
 import importlib
 import os
 
+os.environ.setdefault('SETTINGS_MODULE', 'config.settings.local')
+
 
 class Settings:
     def __init__(self):
         self._setup()
 
     def _setup(self):
-        settings_module = self._get_settings_module()
+        settings_module = os.getenv('SETTINGS_MODULE')
         mod = importlib.import_module(settings_module)
         for config_key in mod.__dir__():
             if config_key.isupper():
                 config_value = getattr(mod, config_key)
                 setattr(self, config_key, config_value)
-
-    @staticmethod
-    def _get_settings_module():
-        try:
-            return os.environ['SETTINGS_MODULE']
-        except KeyError:
-            return 'config.settings.local'
 
 
 settings = Settings()
