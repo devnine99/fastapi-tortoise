9d966a0a6ece326e0ab7111991d7b5e51e499794
devnine99 <devnine99@gmail.com>
2021-12-13 01:42:19 +0900

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,12 @@
 import os
 
-os.environ.setdefault('SETTINGS_MODULE', 'config.settings.local')
-
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
 from api.v1 import router as v1_router
 from config.settings import settings
 
+os.environ.setdefault('SETTINGS_MODULE', 'config.settings.local')
 
 app = FastAPI(title='FastAPI')
 app.include_router(v1_router)
